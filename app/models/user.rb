d321cf740780c57f4a732bef569298c2e7df2b5e
rails udemy course <john.s.swanson.mail@gmail.com>
2017-08-08 19:52:49 +0000

    fixed bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,17 @@ class User < ActiveRecord::Base
          has_many :friendships
          has_many :friends, through: :friendships
          
-         def full_name
+        def full_name
            return "#{first_name} #{last_name}".strip if (first_name || last_name)
            "Anonymous"
-         end
+        end
          
-         def can_add_stock?(ticker_symbol)
+        def can_add_stock?(ticker_symbol)
            under_stock_limit? && !stock_already_added?(ticker_symbol)
            
-         end
+        end
         def under_stock_limit?
           (user_stocks.count < 10)
-          
         end
         def stock_already_added?(ticker_symbol)
           stock = Stock.find_by_ticker(ticker_symbol)
@@ -27,50 +26,27 @@ class User < ActiveRecord::Base
           user_stocks.where(stock_id: stock.id).exists?
         end
         def not_friends_with?(friend_id)
-
-friendships.where(friend_id: friend_id).count < 1
-
-end
-
-def except_current_user(users)
-
-users.reject {|user| user.id == self.id}
-
-end
-
-def self.search(param)
-
-return User.none if param.blank?
-
-param.strip!
-
-param.downcase!
-
-(first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
-
-end
-
-def self.first_name_matches(param)
-
-matches('first_name', param)
-
-end
-
-def self.last_name_matches(param)
-
-matches('last_name', param)
-
-end
-
-def self.email_matches(param)
-
-matches('email', param)
-
-end
-
-def self.matches(field_name, param)
-
-where("lower(#{field_name}) like ?", "%#{param}%")
-
-end
+          friendships.where(friend_id: friend_id).count < 1
+        end
+        def except_current_user(users)
+          users.reject {|user| user.id == self.id}
+        end
+        def self.search(param)
+          return User.none if param.blank?
+          param.strip!
+          param.downcase!
+         (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+        end
+        def self.first_name_matches(param)
+          matches('first_name', param)
+        end
+        def self.last_name_matches(param)
+          matches('last_name', param)
+        end
+        def self.email_matches(param)
+         matches('email', param)
+        end
+        def self.matches(field_name, param)
+        where("lower(#{field_name}) like ?", "%#{param}%")
+        end
 end
